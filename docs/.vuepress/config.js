929dd44ce70429a1ab34e9b7e0b226ae8df720b6
lihu <lihu@igetcool.com>
2022-01-05 19:33:26 +0800

    '初始化目录结构'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,41 +1,70 @@
 module.exports = {
-    title: "summerheart's blog",
+    title: "SummerHeart的博客",
     description: '一位中年大叔的博客',
     themeConfig: {
         //导航
         nav: [
             { text: '首页', link: '/' },
-            { text: 'Guide', link: '/guide/' },
             {
-                text: 'Java',
-                ariaLabel: 'java目录',
+                text: '扎实基础',
+                ariaLabel: 'Java相关',
                 items: [
-                  { text: 'JVM', link: '/java/a/' },
-                  { text: 'B', link: '/java/b/' }
+                  { text: 'Java基础', link: '/java/base/' },
+                  { text: 'Java集合框架', link: '/java/collection/' },
+                  { text: 'Java虚拟机', link: '/java/jvm/Java发展大事件' },
+                  { text: '多线程编程', link: '/java/multithread/' }
                 ]
             },
-            { text: 'External', link: 'https://google.com' },
             {
-                text: 'Languages',
-                ariaLabel: 'Language Menu',
+                text: '框架扩展',
+                ariaLabel: '框架相关',
                 items: [
-                  { text: 'Chinese', link: '/language/chinese/' },
-                  { text: 'Japanese', link: '/language/japanese/' }
+                  { text: 'Spring框架', 
+                    items: [
+                        { text: 'Spring Core', link: '/framework/spring-core/'},
+                        { text: 'Spring MVC', link: '/framework/spring-mvc/' },
+                        { text: 'Spring Security', link: '/framework/spring-security/' },
+                        { text: 'Spring Boot', link: '/framework/spring-boot/' },
+                        { text: 'Spring Cloud', link: '/framework/spring-cloud/' },
+                    ]
+                  },
+                  { text: '其他', 
+                    items:[
+                        { text: 'MyBatis', link: '/framework/mybatis/' },
+                        { text: 'Dubbo', link: '/framework/dubbo/' },
+                        { text: 'Apollo', link: '/framework/apollo/' }
+                    ]
+                  }
                 ]
-            }
+            },
+            { text: '算法', ariaLabel: '算法与数据结构相关', link: '/algorithm/' },
+            { text: 'GitHub', link: 'https://github.com/yihusitian/summerheart-blog' }
         ],
         sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
         //侧边栏
         sidebar: {
-            '/java/a/': [
-                '',
-                'A'
-            ],
-            '/java/b/': [
-                '',
-                'B'
+            '/java/jvm/': getJVMSideBarConfig(),
+        }
+    }
+}
+
+
+// java-develop-jvm
+function getJVMSideBarConfig() {
+    return [
+        {
+            title: "Java虚拟机笔记整理",
+            collapsable: false,
+            sidebarDepth: 0,
+            children: [
+                "Java发展大事件.md",
+                "类加载子系统.md",
+                "运行时数据区.md",
+                "垃圾收集器.md",
+                "深入剖析字节码.md",
+                "虚拟机调优.md",
+                "虚拟机面试题.md",
             ]
         }
-
-    }
+    ]
 }
