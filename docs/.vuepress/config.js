01672dd90d6aa3d449a41a4fa9f7db907c3c841d
lihu <lihu@igetcool.com>
2022-01-05 18:07:02 +0800

    '初始化博客'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,41 @@
 module.exports = {
     title: "summerheart's blog",
-    description: '一位中年大叔的博客'
+    description: '一位中年大叔的博客',
+    themeConfig: {
+        //导航
+        nav: [
+            { text: '首页', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            {
+                text: 'Java',
+                ariaLabel: 'java目录',
+                items: [
+                  { text: 'JVM', link: '/java/a/' },
+                  { text: 'B', link: '/java/b/' }
+                ]
+            },
+            { text: 'External', link: 'https://google.com' },
+            {
+                text: 'Languages',
+                ariaLabel: 'Language Menu',
+                items: [
+                  { text: 'Chinese', link: '/language/chinese/' },
+                  { text: 'Japanese', link: '/language/japanese/' }
+                ]
+            }
+        ],
+        sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+        //侧边栏
+        sidebar: {
+            '/java/a/': [
+                '',
+                'A'
+            ],
+            '/java/b/': [
+                '',
+                'B'
+            ]
+        }
+
+    }
 }
